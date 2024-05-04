e3a1ab434d1913c92c1b78ae80049d4dcbbf491e
Astro <2vvoropaevv2@gmail.com>
2024-05-04 07:14:53 +0700

    Add functions promotion and demotion of the administrator

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,11 +1,11 @@
 from typing import Any
 from uuid import UUID
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.exc import IntegrityError
 from starlette import status
 
-from app.api.deps import SessionDep, UserAuthDep
+from app.api.deps import SessionDep, UserAuthDep, get_current_active_superadmin
 from app.api.models import (
     DeleteUserResponse,
     ShowUser,
@@ -64,6 +64,75 @@ async def delete_user(
             detail=f"User with id {user_id} not found",
         ) from None
     return DeleteUserResponse(deleted_user_id=deleted_user_id)
+
+
+@user_router.patch(
+    "/admin_privilege",
+    response_model=UpdatedUserResponse,
+    dependencies=[Depends(get_current_active_superadmin)],
+)
+async def grant_admin_privilege(
+    user_id: UUID,
+    db: SessionDep,
+    current_user: UserAuthDep,
+):
+    if current_user.user_id == user_id:
+        raise HTTPException(
+            status_code=400, detail="Cannot manage privileges of itself."
+        )
+    user_for_promotion = await _get_user_by_id(user_id, db)
+    if user_for_promotion.is_admin or user_for_promotion.is_superadmin:
+        raise HTTPException(
+            status_code=409,
+            detail=f"User with id {user_id} already promoted to admin / superadmin.",
+        )
+    if user_for_promotion is None:
+        raise HTTPException(
+            status_code=404, detail=f"User with id {user_id} not found."
+        )
+    updated_user_params = {"roles": user_for_promotion.promote_to_admin()}
+    try:
+        updated_user_id = await _update_user(
+            updated_user_params=updated_user_params, session=db, user_id=user_id
+        )
+    except IntegrityError as err:
+        raise HTTPException(status_code=503, detail=f"Database error: {err}")
+    return UpdatedUserResponse(updated_user_id=updated_user_id)
+
+
+@user_router.delete(
+    "/admin_privilege",
+    response_model=UpdatedUserResponse,
+    dependencies=[Depends(get_current_active_superadmin)],
+)
+async def revoke_admin_privilege(
+    user_id: UUID,
+    db: SessionDep,
+    current_user: UserAuthDep,
+):
+    if current_user.user_id == user_id:
+        raise HTTPException(
+            status_code=400, detail="Cannot manage privileges of itself."
+        )
+    user_for_revoke_admin_privileges = await _get_user_by_id(user_id, db)
+    if not user_for_revoke_admin_privileges.is_admin:
+        raise HTTPException(
+            status_code=409, detail=f"User with id {user_id} has no admin privileges."
+        )
+    if user_for_revoke_admin_privileges is None:
+        raise HTTPException(
+            status_code=404, detail=f"User with id {user_id} not found."
+        )
+    updated_user_params = {
+        "roles": user_for_revoke_admin_privileges.remove_admin_privilege()
+    }
+    try:
+        updated_user_id = await _update_user(
+            updated_user_params=updated_user_params, session=db, user_id=user_id
+        )
+    except IntegrityError as err:
+        raise HTTPException(status_code=503, detail=f"Database error: {err}")
+    return UpdatedUserResponse(updated_user_id=updated_user_id)
 
 
 @user_router.get("/", response_model=ShowUser)
--- app/db/models.py
+++ app/db/models.py
@@ -28,3 +28,19 @@ class User(Base):
     is_active: Mapped[bool] = mapped_column(default=True)
     hashed_password: Mapped[str] = mapped_column(String(), nullable=False)
     roles: Mapped[list[str]] = mapped_column(ARRAY(String()), nullable=False)
+
+    @property
+    def is_superadmin(self):
+        return PortalRole.ROLE_PORTAL_SUPERADMIN in self.roles
+
+    @property
+    def is_admin(self):
+        return PortalRole.ROLE_PORTAL_ADMIN in self.roles
+
+    def promote_to_admin(self):
+        if not self.is_admin:
+            return {*self.roles, PortalRole.ROLE_PORTAL_ADMIN}
+
+    def remove_admin_privilege(self):
+        if self.is_admin:
+            return {role for role in self.roles if role != PortalRole.ROLE_PORTAL_ADMIN}
